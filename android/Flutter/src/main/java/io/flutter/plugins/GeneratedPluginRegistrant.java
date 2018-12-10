1b49e63b6eb011f65d8aeb402752825ad844d2ea
zhaoya <zhaoya188@qq.com>
2018-12-10 10:37:22 +0800

    Config an existing app for `add2app`
    Signed-off-by: zhaoya <zhaoya188@qq.com>

--- android/Flutter/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/Flutter/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,23 +1,30 @@
 package io.flutter.plugins;
 
+import com.pichillilorenzo.flutter_inappbrowser.InAppBrowserFlutterPlugin;
+
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
+import io.github.ponnamkarthik.toast.fluttertoast.FluttertoastPlugin;
 
 /**
  * Generated file. Do not edit.
  */
 public final class GeneratedPluginRegistrant {
-  public static void registerWith(PluginRegistry registry) {
-    if (alreadyRegisteredWith(registry)) {
-      return;
+    public static void registerWith(PluginRegistry registry) {
+        if (alreadyRegisteredWith(registry)) {
+            return;
+        }
+        InAppBrowserFlutterPlugin.registerWith(registry.registrarFor("com.pichillilorenzo.flutter_inappbrowser.InAppBrowserFlutterPlugin"));
+        FluttertoastPlugin.registerWith(registry.registrarFor("io.github.ponnamkarthik.toast.fluttertoast.FluttertoastPlugin"));
+        WebViewFlutterPlugin.registerWith(registry.registrarFor("io.flutter.plugins.webviewflutter.WebViewFlutterPlugin"));
     }
-  }
 
-  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
-    final String key = io.flutter.plugins.GeneratedPluginRegistrant.class.getCanonicalName();
-    if (registry.hasPlugin(key)) {
-      return true;
+    private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+        final String key = io.flutter.plugins.GeneratedPluginRegistrant.class.getCanonicalName();
+        if (registry.hasPlugin(key)) {
+            return true;
+        }
+        registry.registrarFor(key);
+        return false;
     }
-    registry.registrarFor(key);
-    return false;
-  }
 }
